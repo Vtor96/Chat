4186b9c445a77511b13733b05331cf20ccf310fd
chhkl <chhkl@mail.ru>
2021-04-30 16:40:33 +0300

    first commit

---
+++ src/client/Client.java
@@ -0,0 +1,50 @@
+package client;
+
+//1) Спросить у подключившегося пользователя его имя.
+//2) Рассылая сообщения необходимо писать имя того, от кого сообщение.
+//3) Не присылать сообщение клиенту который его отправил
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+import java.util.Scanner;
+
+public class Client {
+    public static void main(String[] args){
+        try {
+            Socket socket = new Socket("192.168.0.106", 8188); //45.80.70.161
+
+            System.out.println("Подключился");
+
+            DataInputStream in = new DataInputStream(socket.getInputStream());
+            DataOutputStream out = new DataOutputStream(socket.getOutputStream());
+
+            String response = in.readUTF(); //Ждем сообщение от сервера
+            System.out.println("Ответ от сервера: " + response);
+            Scanner scanner = new Scanner(System.in);
+
+            Thread thread = new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    while (true) {
+                        try {
+                            String response = in.readUTF();
+                            System.out.println(response);
+                        } catch (IOException exception) {
+                            exception.printStackTrace();
+                        }
+                    }
+                }
+            });
+            thread.start();
+
+            while (true) {
+               String request = scanner.nextLine();
+               out.writeUTF(request);
+           }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
